7795f400ac7d8f6262445a9bfb60984763f0575f
SENS/A <151548889+Sensabg@users.noreply.github.com>
2023-12-18 00:03:02 +0200

    Create Messages4.java

---
+++ BasicSyntaxConditionalStatementsAndLoopsMoreExercise/Messages4.java
@@ -0,0 +1,54 @@
+package BasicSyntaxConditionalStatementsAndLoopsMoreExercise;
+
+import java.util.Scanner;
+
+public class Messages4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        int alphabetLength = alphabet.length();
+        int n = Integer.parseInt(scanner.nextLine());
+
+        String finalInput = "";
+
+        for (int i = 0; i < n; i++) {
+            String numbers = scanner.nextLine();
+            int numberLength = numbers.length();
+            int digit = Integer.parseInt(numbers);
+
+            int delete = 0;
+
+            if (numberLength == 1) {
+                delete = 6;
+            } else if (numberLength == 2) {
+                digit /= 10;
+                delete = 5;
+            } else if (numberLength == 3) {
+                digit /= 100;
+                delete = 4;
+            } else if (numberLength == 4) {
+                digit /= 1000;
+                delete = 3;
+            }
+
+            int offset = digit * 3 - delete;
+
+            if (digit > 7 && digit <= 9) {
+                offset++;
+            }
+
+            if (digit == 0) {
+                finalInput += " ";
+                continue;
+            }
+
+            for (int k = offset; k <= alphabetLength - 1; k++) {
+                char alphabetic = alphabet.charAt(k);
+                finalInput += alphabetic;
+                break;
+            }
+        }
+        System.out.println(finalInput);
+    }
+}
