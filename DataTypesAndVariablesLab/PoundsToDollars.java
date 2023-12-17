6f0b34824eb190813cfcf9a1fdbe1a65b61d4ac3
SENS/A <151548889+Sensabg@users.noreply.github.com>
2023-12-18 00:53:40 +0200

    Update PoundsToDollars.java

--- DataTypesAndVariablesLab/PoundsToDollars.java
+++ DataTypesAndVariablesLab/PoundsToDollars.java
@@ -6,7 +6,7 @@ public class PoundsToDollars {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int pounds = Integer.parseInt(scanner.nextLine());
+        double pounds = Double.parseDouble(scanner.nextLine());
         double dollars = pounds * 1.36;
 
         System.out.printf("%.3f",dollars);
