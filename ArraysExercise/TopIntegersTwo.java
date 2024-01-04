c1f137c3b2e9f592bf3e68187a435ceda2c26b65
SENS/A <151548889+Sensabg@users.noreply.github.com>
2024-01-04 17:54:18 +0200

    Add files via upload

---
+++ ArraysExercise/TopIntegersTwo.java
@@ -0,0 +1,24 @@
+package ArraysExercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TopIntegersTwo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int lastElementIndex = numbers.length - 1;
+
+        for (int j = 0; j < lastElementIndex; j++) {
+            if (numbers[j] > numbers[j + 1]) {
+                System.out.print(numbers[j] + " ");
+            }
+        }
+        System.out.println(numbers[lastElementIndex]);
+    }
+}
